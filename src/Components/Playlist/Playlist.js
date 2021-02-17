d345633b6f4b97b9a38188d7a169a60d8867a892
Roger Zavala <rogeliozavala15@gmail.com>
2021-02-17 10:25:03 -0800

    created tracklist component and added to searchresults/playlist

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import TrackList from '../TrackList/TrackList';
 import './Playlist.css';
 
 export default class Playlist extends React.Component{
     render(){
         return (
-            <div class="Playlist">
+            <div className="Playlist">
                 <input value="New Playlist"/>
-                {/* <!-- Add a TrackList component --> */}
-                <button class="Playlist-save">SAVE TO SPOTIFY</button>
+                <TrackList></TrackList>
+                <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
     }
