abf6066a0f853ba958213b4489362112d59d7521
Roger Zavala <rogeliozavala15@gmail.com>
2021-02-17 08:49:37 -0800

    added searchbar along with css

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import SearchBar from '../SearchBar/SearchBar'
 import './App.css'
 
 export default class App extends React.Component{
@@ -8,7 +9,7 @@ export default class App extends React.Component{
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-            {/* <!-- Add a SearchBar component --> */}
+            <SearchBar> </SearchBar>
           <div className="App-playlist">
             {/* <!-- Add a SearchResults component --> */}
             {/* <!-- Add a Playlist component --> */}
