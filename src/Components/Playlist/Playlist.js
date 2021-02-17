7f80902546239b89249d37d6be9cae2d95336407
Roger Zavala <rogeliozavala15@gmail.com>
2021-02-17 10:39:53 -0800

    fixed issues with prop inside playlist

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -6,7 +6,7 @@ export default class Playlist extends React.Component{
     render(){
         return (
             <div className="Playlist">
-                <input value="New Playlist"/>
+                <input defaultValue={'New Playlist'}/>
                 <TrackList></TrackList>
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
